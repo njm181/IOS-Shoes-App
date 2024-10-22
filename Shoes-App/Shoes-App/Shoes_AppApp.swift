cfcef3a3e546baaac354ff1327c91fa21fdbc543
Nicolas Molina <nicolasjmolina1@gmail.com>
2024-10-21 21:52:53 -0300

    Initial commit

---
+++ Shoes-App/Shoes-App/Shoes_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shoes_AppApp.swift
+//  Shoes-App
+//
+//  Created by Nico Molina on 21/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Shoes_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
